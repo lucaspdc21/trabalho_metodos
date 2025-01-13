0b8b243a7d0088b3b074222c73e72f7d78126563
Theosly <theoamagalhaespc@gmail.com>
2025-01-13 18:50:26 -0300

    Update newton_raphson.hpp
    funções adicionadas

--- newton_raphson.hpp
+++ newton_raphson.hpp
@@ -6,8 +6,17 @@
 #define MAX_ITER 100
 using namespace std; 
 
-pair<double, map<int, vector<double>>> newton_raphson (	double (*func) (double),
-						double (*dfunc) (double),
+// f(d) = a*ed – 4*d2
+//f(x) here
+double func (double x,double a){
+   return a * std::exp(x) - 4*(x*x);
+}
+//g(x) = derivative of f(x)
+double derivative (double x,double a){
+   return a * std::exp(x) - 8*x;
+}
+pair<double, map<int, vector<double>>> newton_raphson (	double (*f) (double),
+						double (*df) (double),
 						double x0, double a,
 						double episilon1,
 						double episilon2){
@@ -17,16 +26,16 @@ pair<double, map<int, vector<double>>> newton_raphson (	double (*func) (double),
 	int iter = 0;
 	//começamos do x0 e inserimos a primeira iteracao no Mapa
 	Map_NR.insert(
-      pair<int, vector<double>>(iter, {x0, func(x0), dfunc(x0), x0}));
+      pair<int, vector<double>>(iter, {x0, func(x0, a), derivative(x0, a), x0}));
 	//checamos a condição inicial
-	if (abs(func(x0)) < episilon1) { // condição inicial
+	if (abs(func(x0, a)) < episilon1) { // condição inicial
     return make_pair(x0, Map_NR);
   }
 	//repetimos
 	while (1){
 		iter++;
-		double fx = func(x0); //calculando funcao no ponto 
-		double fpx = func(x0); //calculando derivada da funcao no ponto
+		double fx = func(x0, a); //calculando funcao no ponto 
+		double fpx = func(x0, a); //calculando derivada da funcao no ponto
 		double x = x0 - fx/fpx;  // calculando a raiz aproximada
    		double er = abs(x - x0) / abs(x0); // calculando o erro relativo
 		Map_NR.insert(pair<int, vector<double>>(iter, {x0, a, fx, fpx, er})); // inserindo esses valores no mapa
