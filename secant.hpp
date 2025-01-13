8d82984cee2949f98902d327482f5ea611330052
Theosly <theoamagalhaespc@gmail.com>
2025-01-13 18:50:04 -0300

    Update secant.hpp
    mapa adicionado

--- secant.hpp
+++ secant.hpp
@@ -1,31 +1,52 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <map> 
+#include <utility>
 #define MAX_ITER 100
-double secant (	double (*func) (double),
+using namespace std;
+
+// f(d) = a*ed – 4*d2
+//f(x) here
+double func (double x,double a){
+   return a * std::exp(x) - 4*(x*x);
+}
+//g(x) = derivative of f(x)
+double derivative (double x,double a){
+   return a * std::exp(x) - 8*x;
+}
+
+pair<double, map<int, vector<double>>> secant (	double (*f) (double),
 				double x0,
 				double x1,
+				double a,
 				double episilon1,
 				double episilon2){
+	map<int, vector<double>> Map_SEC;
 	double root,x2;
-	if (abs(func(x0))<episilon1){
-		return x0;
-	}
-	if ((abs(func(x1))<episilon1)||(abs(x1-x0)<episilon2)){
-		return x1;
-	}
+	double fx0 = func(x0, a); 
+	double fx1 = func(x1, a); 
 	int iter = 0;
+	if (abs(fx0)<episilon1){
+		return make_pair(x0, Map_SEC);
+	}
+	if ((abs(fx1)<episilon1)||(abs(x1-x0)<episilon2)){
+		return make_pair(x1, Map_SEC);
+	}
 	while (1){
-		x2 = x1 - (func(x1)/((func(x1)-func(x0))/(x1-x0)));
+		iter++;
+		double fx0 = func(x0, a); 
+		double fx1 = func(x1, a); 
+		x2 = (x0*fx1 - x1*fx0)/(fx1 - fx0); //mudei pra formula do livro só porque... sim? acho ela mais entendível. 
 		double absDif = abs(x2-x1);
-		double funcx2 = func(x2);
-		printf("iter: %i\na(k+1):%.4f\n|a(k+1)-a(k)|: %.4f\nf(a(k+1)):%.4f\n\n",iter,x2,absDif,funcx2);
-		if ((abs(func(x2))<episilon1)||(absDif<episilon2)||(iter>=MAX_ITER)){
-			root = x2;
-			break;
+		double funcx2 = func(x2, a);
+		double erro_relativo = abs(x1 - x0) / abs(x1);
+		Map_SEC.insert(
+        pair<int, vector<double>>(iter, {x0,fx0,x1, fx1, erro_relativo}));
+		if (((abs(funcx2))<episilon1)||(absDif<episilon2)||(iter>=MAX_ITER)){
+			return make_pair(x2, Map_SEC);
 		}
 		x0 = x1;
 		x1 = x2;
-		iter++;
 	}
-	return root;
 }
