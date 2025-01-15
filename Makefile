1bd427e48b903b89222a9062fd09e182fe567afc
itznokx <dimitri.b.medeiros@gmail.com>
2025-01-15 10:56:34 -0300

    Fixing iterface with 2 or less args

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	g++ -o3 -Wextra -Wall main.cpp -o main
--- main.cpp
+++ main.cpp
@@ -5,10 +5,11 @@
 #include <map>
 #include <cstdlib>
 #include <cstring>
-#include "newton_raphson.hpp"
-#include "newton_raphson_modified.hpp"
-#include "secant.hpp"
-using namespace std;
+#include <typeinfo>
+#include "headers/function.hpp"
+#include "headers/newton_raphson.hpp"
+#include "headers/newton_raphson_modified.hpp"
+#include "headers/secant.hpp"
 void openLO(const std::string& scriptPath) {
     // erro: Warning: failed to launch javaldx - java may not function correctly
     // TODO: procuarar uma solução para o erro acima
@@ -20,31 +21,40 @@ void openLO(const std::string& scriptPath) {
         std::cerr << "Erro ao abrir o LibreOffice Calc.\n";
     }
 }
-
-double func (double x,double a){
-    return a * exp(x) - 4*(x*x);
-}
-
-double derivative (double x,double a){
-    return a * exp(x) - 8*x;
-}
-
 int main(int argc, char* argv[]) {
-
+    double x0,x1,a,episilon1,episilon2;
     // --help flag (could be done better, but this will work)
-    if (argc == 2 && strcmp(argv[1], "--help")==0) {
-        cout << "\nCalculadora de zeros de raízes\n";
-        cout << "------------------------------\n";
-        cout << "Comando correto\n";
-        cout << "./main <x0> <x1> <a> <episilon1> <episilon2>\n\n";
+    if (argc < 2){
+        std::cout   << "Input Mode \n" << " \"Use ./main --help\" para utilizar o modo de input direto da linha de comando\n\n";
+        std::cout   << "x0: ";
+        std::cin    >> x0;
+        std::cout   << "x1: ";
+        std::cin    >> x1;
+        std::cout   << "a: ";
+        std::cin    >> a;
+        std::cout   << "episilon1: ";
+        std::cin    >> episilon1;
+        std::cout   << "episilon2: ";
+        std::cin    >> episilon2;
+    }
+    if (argc == 2) {
+        if (std::strcmp(argv[1],"--help")==0){
+            std::cout << "\nCalculadora de zeros de raízes\n";
+            std::cout << "------------------------------\n";
+            std::cout << "Comando correto\n";
+            std::cout << "./main <x0> <x1> <a> <episilon1> <episilon2> <optional flags>\n\n";
+        }
+        else {
+            std::cout << "Utilize a flag --help para saber o comando correto de inserção por linha de comando.\n";
+        }
     }
     else {
         // "atof" function transform char variables to double
-        double x0 = atof(argv[1]);
-        double x1 = atof(argv[2]);
-        double a = atof(argv[3]);
-        double episilon1 = atof(argv[4]);
-        double episilon2 = atof(argv[5]);
+        x0 = stod(argv[1]);
+        x1 = stod(argv[2]);
+        a = stod(argv[3]);
+        episilon1 = stod(argv[4]);
+        episilon2 = stod(argv[5]);
 
         // Saving the funcion results
         pair<double, map<int, vector<double>>> nr = newton_raphson(func, derivative, x0, a, episilon1, episilon2);
